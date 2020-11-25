727152edd8e98c66e010bf62a4b2228fec0ef887
ediedits <74996663+ediedits@users.noreply.github.com>
2020-11-24 22:03:33 -0300

    Add files via upload

---
+++ equacaofistgrau.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int contador;
+    int numero = 1;
+    for (contador = 1; contador <= 10; contador = contador + 1)  {
+
+        printf("mais um\n");
+}
+
+    return 0;
+
+}
